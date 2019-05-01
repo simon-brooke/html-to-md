10d8574ace9da64fc18a0f99d8bead7607c8b853
Simon Brooke <simon@journeyman.cc>
2019-05-01 14:11:36 +0100

    Upversioned to 0.4.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject html-to-md "0.3.0"
+(defproject html-to-md "0.4.0-SNAPSHOT"
     :description "Convert (Enlivened) HTML to markdown; but, more generally, a framework for [HT|SG|X]ML transformation."
     :url "https://github.com/simon-brooke/html-to-md"
     :license {:name "Eclipse Public License"
